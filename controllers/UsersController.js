d96cb25ce96ed27b4412353b5983b18e50d7c507
beboo22 <mt557829@gmail.com>
2024-03-09 23:42:33 +0200

    task

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,24 +2,24 @@ import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 class UsersController {
-  static async postNew(req, res) {
-    // console.log('Request body:', req.body); identify if the request 
-    // body is reaching the handler with the expected data. 
+  static async postNew (req, res) {
+    // console.log('Request body:', req.body); identify if the request
+    // body is reaching the handler with the expected data.
     const { email, password } = req.body;
 
-    if (!email) return res.status(400).send({error: 'Missing email'});
-    if (!password) return res.status(400).send({error: 'Missing password'});
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
 
     const existEmail = await dbClient.userscollection.findOne({ email });
-    
-    if (existEmail) return res.status(400).send({error: 'Email already exists'});
-    
+
+    if (existEmail) return res.status(400).send({ error: 'Email already exists' });
+
     const hashedPassword = sha1(password);
     let result;
     try {
       result = await dbClient.userscollection.insertOne({
         email,
-        password: hashedPassword,
+        password: hashedPassword
       });
     } catch (err) {
       console.error('Error creating user:', err);
@@ -27,7 +27,7 @@ class UsersController {
     }
     const user = {
       id: result.insertedId,
-      email,
+      email
     };
 
     return res.status(201).send(user);
