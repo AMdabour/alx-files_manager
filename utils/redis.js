6b2d1a34472507d12a149582522af01ba63343c6
a-dabour <ah910905@gmail.com>
2024-03-09 21:42:18 +0200

    RedisClient class

---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+import { promisify } from 'util';
+
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.isConnected = true;
+
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+
+    this.client.on('error', (err) => {
+      console.error(`Redis error: ${err.message}`);
+      this.isConnected = false;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
