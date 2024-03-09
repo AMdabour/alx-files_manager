2e43361ce25e18442b8931368b96259ada686eec
a-dabour <ah910905@gmail.com>
2024-03-09 22:20:16 +0200

    API

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import makeRoutes from './routes/index';
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+makeRoutes(server);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default server;
