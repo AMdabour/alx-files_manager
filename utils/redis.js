70151c89ea8e16b79061158a2e70725ece25e722
beboo22 <mt557829@gmail.com>
2024-03-10 16:58:14 +0200

    task

--- utils/redis.js
+++ utils/redis.js
@@ -7,7 +7,7 @@ const redis = require('redis');
  * Class for performing operations with Redis service
  */
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
     this.isConnected = true;
 
@@ -24,14 +24,14 @@ class RedisClient {
   /**
    * Checks if connection to Redis is Alive
    */
-  isAlive () {
+  isAlive() {
     return this.isConnected;
   }
 
   /**
    * Creates a new key in redis with a specific TTL
    */
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     const setAsync = promisify(this.client.set).bind(this.client);
     await setAsync(key, value, 'EX', duration);
   }
@@ -39,7 +39,7 @@ class RedisClient {
   /**
    * gets value corresponding to key in redis
    */
-  async get (key) {
+  async get(key) {
     const getAsync = promisify(this.client.get).bind(this.client);
     return getAsync(key);
   }
@@ -47,7 +47,7 @@ class RedisClient {
   /**
    * Deletes key in redis service
    */
-  async del (key) {
+  async del(key) {
     const delAsync = promisify(this.client.del).bind(this.client);
     return delAsync(key);
   }
